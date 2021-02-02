7a0368218e8722b52a049f347354a5ddf75cac0d
sergeevdevel <vladdementyev@ya.ru>
2021-02-02 11:47:19 +0300

    автозагрузка

--- core/Autoload.php
+++ core/Autoload.php
@@ -1,1 +1,21 @@
 <?php 
+
+function GetListFiles($folder,&$all_files){
+	$fp=opendir($folder);
+	while($cv_file=readdir($fp)){
+		if(is_file($folder."/".$cv_file)){
+			$all_files[]=$folder."/".$cv_file;
+		}
+		elseif($cv_file!="."&&$cv_file!=".."&&is_dir($folder."/".$cv_file)){
+			GetListFiles($folder."/".$cv_file,$all_files);
+		}
+	}
+	closedir($fp);
+}
+
+$all_files=array();
+GetListFiles(__DIR__,$all_files);
+
+foreach ($all_files as $file) {
+	require_once $file;
+}
