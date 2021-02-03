a31cc33003f4c8906d8f52d79761caaff9f97295
sergeevdevel <vladdementyev@ya.ru>
2021-02-03 11:36:35 +0300

    автозагрузка

--- core/Autoload.php
+++ core/Autoload.php
@@ -1,21 +1,28 @@
 <?php 
 
-function GetListFiles($folder,&$all_files){
-	$fp=opendir($folder);
-	while($cv_file=readdir($fp)){
-		if(is_file($folder."/".$cv_file)){
-			$all_files[]=$folder."/".$cv_file;
+require_once __DIR__ . '/../vendor/autoload.php';
+
+function GetListFiles($folder, &$all_files)
+{
+	$fp = opendir($folder);
+	while($cv_file = readdir($fp))
+	{
+		if(is_file($folder."/".$cv_file))
+		{
+			$all_files[] = $folder . "/" . $cv_file;
 		}
-		elseif($cv_file!="."&&$cv_file!=".."&&is_dir($folder."/".$cv_file)){
-			GetListFiles($folder."/".$cv_file,$all_files);
+		elseif($cv_file != "." && $cv_file != ".." && is_dir($folder . "/" . $cv_file))
+		{
+			GetListFiles($folder . "/" . $cv_file, $all_files);
 		}
 	}
 	closedir($fp);
 }
 
-$all_files=array();
-GetListFiles(__DIR__,$all_files);
+$all_files = array();
+GetListFiles(__DIR__, $all_files);
 
 foreach ($all_files as $file) {
 	require_once $file;
 }
+
